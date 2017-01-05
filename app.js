f8bdd8f2185d4a9e15cc7b6b94851c608d2349db
CRUD Team <CRUD Team>
2017-01-05 10:15:45 -0500

    fix port 3000

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var User = require('./models/user')
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.use('/', express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -54,4 +55,6 @@ app.get(('/logout'), function(req, res){
 app.use('/app', session_middleware)
 app.use('/app', router_app)
 
-app.listen(3000)
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
