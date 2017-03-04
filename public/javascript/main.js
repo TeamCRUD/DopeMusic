f9419b25589b195a0a1fb9e81f759111360d4232
Jair Pérez <jair.perez@outlook.com>
2017-03-04 09:31:11 -0500

    estilos

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -3,6 +3,7 @@ $(function(){
     var song = $(".song").click(function(){
         var song_name = $(this).attr("id")
         var audio = $("audio")
+        audio.removeClass("display")
         audio[0].pause()
         audio.attr("src", "/songs/"+song_name)
         audio[0].play()
