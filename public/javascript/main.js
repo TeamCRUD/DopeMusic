2db2b5ca8b69a42cd4872413058e85ce12333083
Jair Pérez <jair.perez@outlook.com>
2017-03-10 09:33:34 -0500

    Tamaño de reproductor

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -3,10 +3,13 @@ $(function(){
     var song = $(".song").click(function(){
         var song_name = $(this).attr("id")
         var audio = $("audio")
+        var description = $(".description")
         audio.removeClass("display")
         audio[0].pause()
         audio.attr("src", "/songs/"+song_name)
         audio[0].play()
+
+        description.addClass("display")
     })
 
     var list = $(".song")
