017e56f63d2603c4136e4ed9b36860c59f517889
Jair Pérez <jair.perez@outlook.com>
2017-03-11 18:26:39 -0500

    reproductor

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,17 +2,45 @@ $(function(){
 
     var song = $(".song").click(function(){
         var song_name = $(this).attr("id")
-        var audio = $("audio")
+        var audio = $("#mytrack")
+        var wrapper = $("#wrapper")
         var description = $(".description")
-        audio.removeClass("display")
         audio[0].pause()
         audio.attr("src", "/songs/"+song_name)
         audio[0].play()
 
+        wrapper.removeClass("display")
         description.addClass("display")
     })
 
     var list = $(".song")
 
     list.on("click", song)
+
+    var play = $("#playButton").click(function(){
+        var audio = $("#mytrack")
+        audio[0].pause()
+        var pause = $("#pauseButton")
+        pause.removeClass("display")
+        $(this).addClass("display")
+    })
+
+    var pause = $("#pauseButton").click(function(){
+        var audio = $("#mytrack")
+        audio[0].play()
+        var play = $("#playButton")
+        play.removeClass("display")
+        $(this).addClass("display")
+    })
+
+    var mute = $("#muteButton").click(function(){
+        var audio = $("#mytrack")
+        if(audio[0].muted){
+            audio[0].muted = false
+            $(this).removeClass("icon-mute").addClass("icon-volume")
+        }else{
+            audio[0].muted = true
+            $(this).removeClass("icon-volume").addClass("icon-mute")
+        }
+    })
 })
