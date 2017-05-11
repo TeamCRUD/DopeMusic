d0d723aec03de4e38b6df9e24a86b2589ff6b10c
Jair Perez <jair.perez@outlook.com>
2017-05-11 16:32:50 -0500

    Crear usuarios

--- routes/signup.js
+++ routes/signup.js
@@ -8,20 +8,5 @@ router.route("/")
     .get(function(req, res, next) {
         res.render("signup",{title: "Creators", message: "Datos personales"})
     })
-    .post( function(req, res, next) {
-        res.redirect("/login")
-        /*var user = new User({
-            email: req.body.email,
-            username: req.body.username,
-            password: req.body.password
-        })
-
-        user.save().then(function(us){
-            res.redirect("/login")
-        },function(err){
-            res.render("signup", {title: "Creators", message: "Datos incorrectos"})
-            console.log(err)
-        })*/
-    });
 
 module.exports = router;
